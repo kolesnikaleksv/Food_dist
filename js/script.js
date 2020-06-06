45e2636227c830710808cf0985dee40e3e056eee
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-06-06 21:11:51 +0300

    use rest operator

--- js/script.js
+++ js/script.js
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
         
-        //const modalTimerId = setTimeout(openModal, 5000); // open modal window after 5s
+        const modalTimerId = setTimeout(openModal, 5000); // open modal window after 5s
         
         function showModalByScroll() {
             if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -143,17 +143,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 window.removeEventListener('scroll', showModalByScroll);
             }
         }
-        //window.addEventListener('scroll', showModalByScroll);
+        window.addEventListener('scroll', showModalByScroll);
         
 
         // Used class for cards
 
         class MenuCard {
-            constructor(src, alt, title, descr, price, parentSelector) {
+            constructor(src, alt, title, descr, price, parentSelector, ...classes) {
                 this.src = src;
                 this.alt = alt;
                 this.title = title;
                 this.descr = descr;
+                this.classes = classes;
                 this.price = price;
                 this.parent = document.querySelector(parentSelector);
                 this.transfer = 27;
@@ -166,17 +167,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
             render() {
                 const element = document.createElement('div');
+
+                if (this.classes.length === 0) {
+                    this.element = 'menu__item';
+                    element.classList.add(this.element);
+                } else {
+                    this.classes.forEach(className => element.classList.add(className));
+                }
+                
                 element.innerHTML =  `
-                    <div class="menu__item">
-                        <img src="${this.src}" alt="${this.alt}">
-                        <h3 class="menu__item-subtitle">Меню “${this.title}”</h3>
-                        <div class="menu__item-descr">В меню “${this.title}” ${this.descr}
-                        </div>
-                        <div class="menu__item-divider"></div>
-                        <div class="menu__item-price">
-                            <div class="menu__item-cost">Цена:</div>
-                            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                        </div>
+                    <img src="${this.src}" alt="${this.alt}">
+                    <h3 class="menu__item-subtitle">Меню “${this.title}”</h3>
+                    <div class="menu__item-descr">В меню “${this.title}” ${this.descr}
+                    </div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
                 `;
                 this.parent.append(element);
@@ -188,18 +195,35 @@ document.addEventListener('DOMContentLoaded', () => {
              "https://f1.ds-ua.net/u_dirs/002/2659/0d03f55d7a5632e1f9ae2ecc19da8c15.jpg", 
              "vegy",
              "Ожиряшка",
-            ",мы используем не только много майонеза и жира как наполнитель, но и качественное сало как украшение, которое кстати при этом не содержит  ни капли мяса - это самое белое меню",
+            `,мы используем не только много майонеза и жира как наполнитель, но и 
+            качественное сало как украшение, которое кстати при этом не содержит  
+            ни капли мяса - это самое белое меню`,
             23,
-            ".menu .container"
+            ".menu .container",
+            "menu__item"
         ).render();
         new MenuCard(
             "https://c7.hotpng.com/preview/521/365/115/earthworms-wikia-digital-pet-others.jpg", 
             "vegy",
             "Букашка",
-           ",мы используем не только много жуков и  и мух как наполнитель, но и толстых дождевых червей как украшение, которое кстати при этом содержат много земли при минимуму каллорий  - это самое землистое меню",
+           `,мы используем не только много жуков и  и мух как наполнитель, но и толстых дождевых 
+           червей как украшение, которое кстати при этом содержат много земли при минимуму каллорий 
+           - это самое землистое меню`,
            22,
-           ".menu .container"
-       ).render();
+           ".menu .container",
+           "menu__item"
+        ).render();
+        new MenuCard(
+            "img/tabs/post.jpg", 
+            "vegy",
+            "Постное",
+            `- это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения,
+             молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и
+              импортных вегетарианских стейков.`,
+           22,
+           ".menu .container",
+           "menu__item"
+        ).render();
 
           
 });
